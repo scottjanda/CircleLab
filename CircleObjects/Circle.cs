4c21513c39c336bbb8b42991283ff0d8152cf5be
Janda <SJanda@logility.com>
2023-02-11 19:06:57 -0500

    initial commit

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    public class Circle
+    {
+        double radius;
+
+        public static int counter = 0;
+
+        public Circle(double Radius) {
+            radius= Radius;
+            counter++;
+        }
+        public double CalculateCircumference() { 
+            return 2*radius*Math.PI; }
+        public string CalculateFormattedCircumference() { return $"Circumference: { Math.Round(CalculateCircumference(),2)}"; }
+        public double CalculateArea() { return Math.PI * Math.Pow(radius,2) ; }
+        public string CalculateFormattedArea() { return $"Area:          {Math.Round(CalculateArea(),2)}"; }
+        private string FormatNumber(double x) { return $"Goodby! You created {counter} circle objects!"; }
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,24 @@
+﻿using CircleObjects;
+using System.Diagnostics.Metrics;
+
+Circle circle = new Circle(userRadius);
+string userContinue;
+
+do
+{
+
+Console.WriteLine("Welcome to the circle tester");
+Console.Write("Enter radius:  ");
+bool validRadius = double.TryParse(Console.ReadLine(), out double userRadius);
+
+
+
+Console.WriteLine(circle.CalculateFormattedCircumference());
+Console.WriteLine(circle.CalculateFormattedArea());
+
+Console.Write("Continue? (y/n):");
+    userContinue = Console.ReadLine();
+
+} while (userContinue.ToLower() == "y");
+
+Console.WriteLine($"Goodby! You created {counter} circle objects!");
